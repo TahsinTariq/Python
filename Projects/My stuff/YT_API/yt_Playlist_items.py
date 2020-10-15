a2530e7bc420dee79883a5682fb05adf5c89f2de
Tahsin <62146852+TahsinTariq@users.noreply.github.com>
2020-10-15 19:49:08 +0600

    Update yt_Playlist_items.py

--- Projects/My stuff/YT_API/yt_Playlist_items.py
+++ Projects/My stuff/YT_API/yt_Playlist_items.py
@@ -1,8 +1,9 @@
 from googleapiclient.discovery import build
 import os, sys
 
-NAME = '1-Vectors-The-Nature-of-Code'
-ID = 'PLRqwX-V7Uu6ZwSmtE13iJBcoI-r4y7iEc'
+REMOVE = 'The Nature of Code'
+NAME = '5-Physics Libraries'
+ID = 'PLRqwX-V7Uu6akvoNKE4GAxf6ZeBYoJ4uh'
 API_KEY = 'AIzaSyAuQpsmRRbJgd-0pktJ0bjP8dSwGjVL6rY'
 PATH = r'E:\Software\GitHub Desktop\website\_more\archive\nature-of-code'
 
@@ -16,6 +17,10 @@ def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
 	        print(*map(f, objects), sep=sep, end=end, file=file)
 
 if __name__ == '__main__':
+	os.chdir(f'{PATH}')
+	os.makedirs(f'{NAME}')
+	os.chdir(f'{PATH}/{NAME}')
+
 	youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 	items = youtube.playlistItems().list(
@@ -25,21 +30,45 @@ if __name__ == '__main__':
 				)
 
 	pl_items = items.execute()
-	uprint(pl_items)
+	# uprint(pl_items)
 
-	# for item in pl_items['items']:
-	# 	vid_id = item['contentDetails']['videoId']
-	# 	print(vid_id,end='\n\t')
-	# 	vid_req = youtube.videos().list(
-	# 				# part = 'contentDetails',
-	# 				part = 'snippet',
-	# 				id = vid_id
-	# 			)
-	# 	details = vid_req.execute()['items'][0]['snippet']
-	# 	# uprint(details)
-	# 	uprint(details['publishedAt'][0:10], end='\n\t')
-	# 	uprint(details['title'], end='\n\t')
-	# 	uprint(details['description'].split('\n')[0], end='\n\t')
-	# 	print()
+	for i,item in enumerate(pl_items['items']):
+		vid_id = item['contentDetails']['videoId']
+		# print(vid_id,end='\n\t')
+		vid_req = youtube.videos().list(
+					# part = 'contentDetails',
+					part = 'snippet',
+					id = vid_id
+				)
+		details = vid_req.execute()['items'][0]['snippet']
+		# uprint(details)
+		file_name = details['title'].replace('I.', '0.').replace('?', "").replace(REMOVE, "").replace(': ', '-').replace(' - ', '-').replace(' ', '-')[0:-1]
+		# print(file_name, end='\n\t')
+
+		title = ' '.join(file_name.split('-')[1:])
+		title = title.replace(REMOVE, "")
+		# print(title, end='\n')
+		video_number = i+1
+		date = details['publishedAt'][0:10]
+		video_id = vid_id
+		for line in details['description'].split('\n'):
+			if "This video" or 'this video' in line:
+				description = line
+				break
+		with open(f'{file_name}.md', 'w') as f:
+			f.writelines('---\n')
+			f.writelines(f'title: {title}\n')
+			f.writelines(f'video_number: {video_number}\n')
+			f.writelines(f'date: {date}\n')
+			f.writelines(f'video_id: {video_id}\n')
+			f.writelines('---\n')
+			f.writelines(f'{description}\n')
+
+
+
+		# uprint(details['publishedAt'][0:10], end='\n\t')
+		# uprint(details['title'].replace(': ', '-').replace(' - ', '-').replace(' ', '-'), end='\n\t')
+		# uprint(details['description'].split('\n')[0], end='\n\t')
+		# print()
 
 
