2f50c542478f3f7d981f562667e7a30397d78ee6
Tahsin <62146852+TahsinTariq@users.noreply.github.com>
2020-10-26 19:35:18 +0600

    that's a spicy meatballll!!!!

--- Projects/My stuff/YT_API/yt_api.py
+++ Projects/My stuff/YT_API/yt_api.py
@@ -6,12 +6,13 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 request = youtube.channels().list(
 			part = 'contentDetails,statistics',
-			forUsername='shiffman'
+			# forUsername='shiffman'
+			forUsername='enyay'
 			)
-# print(request.execute()['items'][0]['id'])
-url = request.execute()['items'][0]['id']
-playlist = youtube.playlists().list(
-			part = 'contentDetails, snippet',
-			channelId=url
-			)
-print(playlist.execute())
+print(request.execute()['items'])
+# url = request.execute()['items'][0]['id']
+# playlist = youtube.playlists().list(
+# 			part = 'contentDetails, snippet',
+# 			channelId=url
+# 			)
+# print(playlist.execute())
